1efd94f24e118ff56b69f62dadcdebd4add96db1
Florent <florent.tavernier@gmail.com>
2022-02-28 14:57:38 +0100

    still bug

--- Partie TensorFlow/src/background.js
+++ Partie TensorFlow/src/background.js
@@ -37,80 +37,113 @@ var webHookUrl = "https://discord.com/api/webhooks/945642399584120842/hU9VSm0vuy
 //     return predictionArray
 // }
 
-function test() {
-    var predictionArray = []
-    var img = new Image()
-    img.url = "https://static.actu.fr/uploads/2019/10/poney-metro-paris-ratp-ligne-5-italie.jpeg"
-    nsfwjs.load().then((model) => {
-    model.classify(img).then((predictions) => {
-        predictionArray.push(predictions)
-        console.log("Predictions", predictions);
-        window.alert("Predictions ", predictions)
-    });
-    })
-    return predictionArray
-}
+// function test() {
+//     var predictionArray = []
+//     var img = new Image()
+//     img.url = "https://static.actu.fr/uploads/2019/10/poney-metro-paris-ratp-ligne-5-italie.jpeg"
+//     nsfwjs.load().then((model) => {
+//     model.classify(img).then((predictions) => {
+//         predictionArray.push(predictions)
+//         console.log("Predictions", predictions);
+//         window.alert("Predictions ", predictions)
+//     });
+//     })
+//     return predictionArray
+// }
 
-function sendToDiscord(predictionArray) {
-    postToWebhook("** voilà les scores : **" + predictionArray.join());
-}
+// function sendToDiscord(predictionArray) {
+//     postToWebhook("** voilà les scores : **" + predictionArray.join());
+// }
 
-function postToWebhook(content) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', webHookUrl, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+// function postToWebhook(content) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('POST', webHookUrl, true);
+//     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   
-    data = {
-        content: content,
-    }
+//     data = {
+//         content: content,
+//     }
 
-    xhr.send(JSON.stringify(data));
+//     xhr.send(JSON.stringify(data));
 
-    xhr.onload = function(res) {
-        console.log('posted: ', res);
-    }
+//     xhr.onload = function(res) {
+//         console.log('posted: ', res);
+//     }
 
-    xhr.onerror = function(res) {
-        console.log('error posting: ', res);
-    }
-}
+//     xhr.onerror = function(res) {
+//         console.log('error posting: ', res);
+//     }
+// }
 
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
+// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+//     if (changeInfo.status == 'complete') {
   
-    var img = new Image()
-    img.url = "https://static.actu.fr/uploads/2019/10/poney-metro-paris-ratp-ligne-5-italie.jpeg"
-    nsfwjs.load().then((model) => {
-    model.classify(img).then((predictions) => {
-        console.log("Predictions", predictions);
-        window.alert("Predictions ", predictions)
-    });
-    })
-    }
-})
+//     var img = new Image()
+//     img.url = "https://static.actu.fr/uploads/2019/10/poney-metro-paris-ratp-ligne-5-italie.jpeg"
+//     nsfwjs.load().then((model) => {
+//     model.classify(img).then((predictions) => {
+//         console.log("Predictions", predictions);
+//         window.alert("Predictions ", predictions)
+//     });
+//     })
+//     }
+// })
 
 //window.onload = sendToDiscord(img_analyse())
 // window.onload = sendToDiscord(test())
 
 
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log('Requete : ' + request.greeting)
+    console.log('Sender tab url : ' + sender.tab.url);
+    console.log('Requete : ' + request.imgs[0])
+    var imgSrcs = request.imgs
+    //var Scores = []
+    var imgArray = []
+    for (var i = 0; i < imgSrcs.length; i++) {
+        imgArray[i] = new Image();
+        imgArray[i].src = imgSrcs[i]
+    }
+    console.log(imgArray[4])
+
+    
+    for (var i = 0; i < imgArray.length; i++) {
+        // nsfwjs.load()
+        //     .then(function (model) {
+        //         // Classify the image
+        //         return model.classify(imgArray[i])
+        //     })
+        //     .then(function (predictions) {
+        //         console.log('Predictions: ', predictions)
+        //         console.log(imgArray[i].src)
+        //     })
+
+        nsfwjs.load()
+            .then((model) => {
+            model.classify(imgArray[i])
+            .then((predictions) => {
+            console.log("Predictions", predictions);
+            });
+        })
+        
+    }
+    //sendResponse({farewell: "ok ici le background script", autre: 'lalala'});
+    //return true;
+});
 
 
 
 
-// OTHER SOURCE :
 
-// import * as nsfwjs from 'nsfwjs'
 
-// const img = document.getElementById('img')
 
-// // Load model from my S3.
-// // See the section hosting the model files on your site.
-// const model = await nsfwjs.load()
 
-// // Classify the image
-// const predictions = await model.classify(img)
-// console.log('Predictions: ', predictions)
+
+
+
+
+
 
 
 
--- Partie TensorFlow/src/content.js
+++ Partie TensorFlow/src/content.js
@@ -60,11 +60,68 @@
 
 
 
+window.onload = sendIt()
 
+function sendIt() {
+    var imgsArray = img_find()
+    console.log('src numéro 0 : ' + imgsArray[0])
+    console.log('liste des srcs : ' + imgsArray)
+    
+    chrome.runtime.sendMessage({greeting: "Salut, c'est le content script, voilà les srcs", imgs: imgsArray}, function(response) {
+        //console.log(response.farewell);
+    })
+}
 
+function img_find() {
+    var imgs = document.getElementsByTagName("img");
+    //console.log(imgs[0].getAttribute('data-lazy'))
+    //console.log(imgs[0].getAttribute('data-srcset').split(" ")[0])
+    var imgSrcs = [];
+    if (imgs.length > 0) {
+        //Push dans imgSrcs toutes les images de la page :
+        for (var i = 0; i < imgs.length; i++) {
+            if (imgs[i].getAttribute('src') !== null) {
+                imgSrcs.push(imgs[i].src);
+                console.log('src!')
+            } else if (imgs[i].getAttribute('srcset') !== null) {
+                imgSrcs.push(imgs[i].currentSrc);
+                console.log('srcset!')
+            } else if (imgs[i].getAttribute('data-lazy') !== null) {
+                imgSrcs.push(imgs[i].getAttribute('data-lazy'));
+                console.log('datalazy!')
+            } else if (imgs[i].getAttribute('data-srcset') !== null) {
+                imgSrcs.push(imgs[i].getAttribute('data-srcset').split(" ")[0]);
+                console.log('data-srcset!')
+            } else if (imgs[i].getAttribute('data-src') !== null) {
+                imgSrcs.push(imgs[i].getAttribute('data-src'));
+                console.log('data-src!')
+            } else {
+                console.log('une image na eu ni lun ni lautre : ')
+            }
+        }
+        
+        //Supprime lorsque l'url de l'image commence par un '/' :
+        for (var i = 0; i < imgSrcs.length; i++) {
+            if (imgSrcs[i][0] == '/') {
+                imgSrcs.splice(i, 1);
+                i--
+            }
+        }
 
+        //Supprime les doublons :
+        //console.log('avant suppression : ')
+        //console.log('imgSrc length : ' + imgSrcs.length)
+        //imgSrcs.forEach(e => console.log(e))
+        var imgSrcs = [...new Set(imgSrcs)];
+        console.log('après suppression : ')
+        console.log('imgSrc length : ' + imgSrcs.length)
+        imgSrcs.forEach(e => console.log(e))
 
-
+        //Problème : met plusieurs fois quand même img mais != url comme sur : https://github.com/tensorflow/tfjs/issues/322
+        //Ne fait pas encore les vidéos
+    }
+    return imgSrcs;
+}
 
 
 
