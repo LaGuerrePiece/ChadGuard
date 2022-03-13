a03829aab57930cda02edaf4579e2370df9bf86e
Paul-Louis Charrière <plcharriere@gmail.com>
2022-03-13 13:27:08 +0100

    Typescript project boilerplate

--- README.md
+++
@@ -1,9 +0,0 @@
-<p align="center">
-    <img align="center" src="https://github.com/whiterqbbit/ChadWatch/blob/main/images/chadlook2.png" alt="The Chad Look" width="400" />
-    <h1 align="center">ChadGuard</h1>
-    <h2 align="center">Reveal your inner chad</h2>
-</p>
-
-
-A chrome Extension that tells everyone on [The War Room Discord server](https://discord.gg/d3GDTz6GUD) if you watch porn.
-
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const PATHS = {
+  src: path.join(__dirname, "/src"),
+  dist: path.join(__dirname, "/dist"),
+};
+
+module.exports = {
+  entry: {
+    content: `${PATHS.src}/content.ts`,
+    background: `${PATHS.src}/background.ts`,
+  },
+  output: {
+    path: PATHS.dist,
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(eot|png|svg|[ot]tf|woff2?)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+};
