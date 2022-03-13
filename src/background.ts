5ff14cbab1de2d6827a0418993a7c26973710997
Paul-Louis Charrière <plcharriere@gmail.com>
2022-03-13 19:00:56 +0100

    fix: Removed nsfwjs breaking modules

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,5 @@
 import defaultBlocklist from "./defaultBlocklist.json";
-import * as nsfwjs from "nsfwjs";
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.local.set({ blocklist: defaultBlocklist });
 });
-
-const model = await nsfwjs.load();
