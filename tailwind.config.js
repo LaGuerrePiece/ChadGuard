e4b6f8523c3ae119b1bf3add78c6c9302ed397a7
turboblitz <62038140+0xturboblitz@users.noreply.github.com>
2022-04-06 20:30:20 +0200

    Revert "fix: the pages were blocked in incognito mode"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        main: ['"Proxima Nova"', '"ui-sans-serif"', '"system-ui"', '"-apple-system"', '"BlinkMacSystemFont"', '"Segoe UI"', '"Roboto"', '"Helvetica Neue"', '"Arial"', '"Noto Sans"', '"sans-serif"', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
