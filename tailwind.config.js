803ee9097fa980a35dbcb0b32f04a7dced74f474
maxentr <rouaultmaxent@gmail.com>
2022-04-06 20:23:53 +0200

    add add font to tailwind config (not sure for the name)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ['"Proxima Nova"', '"ui-sans-serif"', '"system-ui"', '"-apple-system"', '"BlinkMacSystemFont"', '"Segoe UI"', '"Roboto"', '"Helvetica Neue"', '"Arial"', '"Noto Sans"', '"sans-serif"', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
+    },
   },
   plugins: [],
 };
